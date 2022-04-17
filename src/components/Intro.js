a125ea7636b5fd2ff498f62225596b109e5d8837
Rashaad Bracley <rsamuelb14@gmail.com>
2022-04-17 12:26:20 -0400

    first commit

---
+++ src/components/Intro.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./intro.css"
+
+const Intro = () => {
+  return (
+    <div className="i">
+  <div className="i-left">
+      <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, My name is </h2>
+          <h1 className="i-name">Rashaad Bracley</h1>
+      </div>
+  </div>
+  <div className="i-right">right</div>
+    
+
+    </div>
+  )
+}
+
+export default Intro
