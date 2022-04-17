2a664f6a10179ab9afac0060cd8a6cd146a919ad
Rashaad Bracley <rsamuelb14@gmail.com>
2022-04-17 12:36:22 -0400

    working on Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,6 +8,11 @@ const Intro = () => {
       <div className="i-left-wrapper">
           <h2 className="i-intro">Hello, My name is </h2>
           <h1 className="i-name">Rashaad Bracley</h1>
+          <div className="i-title">
+              <div className="i-title-wrapper">
+                 <div className="i-title-item">Junior Software Engineer</div> 
+              </div>
+          </div>
       </div>
   </div>
   <div className="i-right">right</div>
