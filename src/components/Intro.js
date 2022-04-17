56c1e00bed182378f3c467dfafa96c87f322eee2
Rashaad Bracley <rsamuelb14@gmail.com>
2022-04-17 15:36:43 -0400

    Intro final look

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,25 +1,36 @@
-import React from 'react'
-import "./intro.css"
+import React from "react";
+import "./intro.css";
+
 
 const Intro = () => {
   return (
     <div className="i">
-  <div className="i-left">
-      <div className="i-left-wrapper">
+      <div className="i-left">
+        <div className="i-left-wrapper">
           <h2 className="i-intro">Hello, My name is </h2>
           <h1 className="i-name">Rashaad Bracley</h1>
           <div className="i-title">
-              <div className="i-title-wrapper">
-                 <div className="i-title-item">Junior Software Engineer</div> 
-              </div>
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Junior Software Engineer</div>
+              <div className="i-title-item">JavaScript</div>
+              <div className="i-title-item">React.js</div>
+              <div className="i-title-item">Node.js</div>
+              <div className="i-title-item">Node.js</div>
+             
+            </div>
           </div>
+          <p className="i-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            
+          </p>
+        </div>
       </div>
-  </div>
-  <div className="i-right">right</div>
-    
-
+      <div className="i-right">
+        <div className="i-bg"></div>
+        <img src="./img/me.jpeg" alt="" className="i-img" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
