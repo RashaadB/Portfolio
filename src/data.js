72cf903550ea584a19d8ae72a2d1d255eadaccf8
Rashaad Bracley <rsamuelb14@gmail.com>
2022-04-25 13:36:59 -0400

    netlify

--- src/data.js
+++ src/data.js
@@ -1,33 +1,33 @@
 export const projects = [
   {
     id: 1,
-    name:"Gateway",
+    name: "Gateway",
     img: "./img/Miami_page.png",
     link: "https://myfirstwebpage.rashaadbracley.repl.co/",
   },
   {
     id: 2,
-    name:"Code For Justice",
+    name: "Code For Justice",
     img: "./img/code_justice.png",
     link: "https://rashaadb.github.io/Code4justice/index.html",
   },
   {
     id: 3,
-    name:"Hope Hacks",
+    name: "Hope Hacks",
     img: "hope hacks",
     link: "hopehacks",
   },
   {
     id: 4,
-    name:"Instagram Redesign",
+    name: "Instagram Redesign",
     img: "./img/instagram_redesign.png",
     link: "https://instagram-redesign-5v9db9j7y-winston1337.vercel.app/home",
   },
   {
     id: 5,
-    name:"Ecommerce",
+    name: "Ecommerce",
     img: "./img/ecom_pic.png",
-    link: "https://exquisite-faloodeh-ab6fc2.netlify.app",
+    link: "https://exquisite-faloodeh-ab6fc2.netlify.app/",
   },
   // {
   //   id: 6,
